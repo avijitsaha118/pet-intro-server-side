53dbe6262e11ad34ac93516e4179f72b418079b7
avijitsaha118 <sahaavijit908@gmail.com>
2022-09-16 08:15:12 +0600

    delete method added

--- index.js
+++ index.js
@@ -3,24 +3,15 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.port || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
 app.use(express.json());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jhuifu9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
 
 
 async function run() {
@@ -38,6 +29,13 @@ async function run() {
         app.post('/information', async (req, res) => {
             const newInformation = req.body;
             const result = await informationCollection.insertOne(newInformation);
+            res.send(result);
+        });
+
+        app.delete('/information/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await informationCollection.deleteOne(query);
             res.send(result);
         });
 
